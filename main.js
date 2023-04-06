d5affa683f97b153967edb3a6573a2d07f6e08a5
pepo999 <127298097+pepo999@users.noreply.github.com>
2023-04-06 16:12:25 +0200

    forked from jing

--- main.js
+++ main.js
@@ -5,16 +5,31 @@
 // 4) aggiunta della data di nascita dello studente
 // 5) se è il compleanno dello studente il suo nome sarà scritto in modo evidenziato
 
+'use strict'
+
 const student1 = new Student('Simone', 'Maccarone');
 const student2 = new Student('Luis Alberto', 'Castro');
 const student3 = new Student('Davide', 'Consigliere');
 const student4 = new Student('Francesco', 'Badile');
 
-const classroom1 = new ClassRoom([student1, student2, student3, student4]);
+const classroom1 = new Classroom([student1, student2, student3, student4]);
 
+let students1 = classroom1.students;
 
-function displayClassroom(classroom){
+console.log(students1);
 
+function displayClassroom(students){
+
+document.getElementById('student-list').innerHTML = '';
+
+for (let i = 0; i < students.length; i++) {
+    const element = students[i];
+    let studentList = document.getElementById('student-list');
+    let newLi = document.createElement('li');
+    studentList.appendChild(newLi);
+    const studentName = document.createTextNode(element.name+' '+element.surname);
+    newLi.appendChild(studentName)
+}
 }
 
 function shuffleTheClassroom(){
@@ -22,7 +37,15 @@ function shuffleTheClassroom(){
 }
 
 function addStudentToClassroom(){
+    const students = classroom1.students
+const inputName = document.getElementById('input-name');
+const inputSurname = document.getElementById('input-surname');
+const newStudent = new Student(inputName.value, inputSurname.value);
+if(inputName.value !== '' && inputSurname.value !== '') {
+    students.push(newStudent);
+    displayClassroom(classroom1.students)
+}
 
 }
 
-
+displayClassroom(students1)
